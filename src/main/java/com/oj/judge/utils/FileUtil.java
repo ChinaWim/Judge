c23169071dbb7ec96ff6ab6e55440cc84dd88687
wim <m969130721@163.com>
2019-10-20 22:31:15 +0800

    better

--- src/main/java/com/oj/judge/utils/FileUtil.java
+++ src/main/java/com/oj/judge/utils/FileUtil.java
@@ -2,6 +2,7 @@ package com.oj.judge.utils;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * @author m969130721@163.com
@@ -15,12 +16,21 @@ public class FileUtil {
         if (!parentFile.exists()) {
             parentFile.mkdirs();
         }
+        FileOutputStream outputStream = null;
         try {
-            FileOutputStream outputStream = new FileOutputStream(file);
+            outputStream = new FileOutputStream(file);
             byte[] bytes = content.getBytes();
             outputStream.write(bytes, 0, bytes.length);
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
